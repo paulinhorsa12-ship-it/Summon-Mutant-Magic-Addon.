00aedcff657981df524e457a87f311191cc7f066
paulinhorsa12-ship-it <paulinhorsa12@gmail.com>
2025-09-17 21:17:16 -0300

    Add files via upload

---
+++ src/main/java/com/darkness/summonmutantmagic/SummonMutantMagicAddon.java
@@ -0,0 +1,13 @@
+
+package com.darkness.summonmutantmagic;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SummonMutantMagicAddon.MODID)
+public class SummonMutantMagicAddon {
+    public static final String MODID = "summonmutantmagic";
+
+    public SummonMutantMagicAddon() {
+        // Inicialização básica do mod
+    }
+}
